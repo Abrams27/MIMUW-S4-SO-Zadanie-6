7c0b36dda28338733135b840a944ed12f4e58c9f
Abrams27 <abramowiczmarcin99@gmail.com>
2020-06-08 17:18:29 +0200

    finalne rozwiazanie

--- minix_source/sync.sh
+++ minix_source/sync.sh
@@ -4,3 +4,6 @@ cd "$(dirname "$0")" # set current working directory to the directory of the scr
 
 rsync -uav --exclude=".git" ./usr/include/ minix:/usr/include/
 rsync -uav --exclude=".git" ./usr/src/ minix:/usr/src/
+scp ioc_example.c minix:~/ioc_example.c
+scp system.conf minix:/etc/system.conf
+scp test.py minix:~/test.py
